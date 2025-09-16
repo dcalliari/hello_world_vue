7cf5ae066c64e8e3d137de7a71deccb94c730eb8
daniel calliari <dbcalliari@gmail.com>
2025-09-15 21:18:01 -0300

    feat: add student list to the Hello World template

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 
 <template>
   <h1>Hello World!</h1>
+  <ul>
+    <li>Carlos Vinícius - 04160432</li>
+    <li>Daniel Calliari – 04152374</li>
+    <li>João Guilherme Cândido - 04158324</li>
+    <li>Lucas Nobrega – 04174176</li>
+    <li>Marcos Rodrigues - 04162413</li>
+    <li>Rafael Roberto - 04156399</li>
+  </ul>
 </template>
 
 <style scoped></style>
